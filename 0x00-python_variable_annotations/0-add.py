e831849f6f02a79de66daef68e82cd9644b844b3
Muigai-ben <muigaibenson37@gmail.com>
2024-04-18 09:27:07 +0000

    annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-''' a python module that returns the addition'''
+
+'''A python module that returns the summation of two float numbers'''
+
 
 def add(a: float, b: float) -> float:
-     '''Float: The sum of a and b.'''
+    '''A function that returns the summation of two float numbers'''
     return a + b
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
+
     print(add(1.11, 2.22) == 1.11 + 2.22)
     print(add.__annotations__)
